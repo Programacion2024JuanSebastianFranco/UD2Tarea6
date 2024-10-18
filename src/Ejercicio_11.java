ce085522e7c750fe1569b604f594c863da998ec5
Franco <juafracas@alu.edu.gva.es>
2024-10-18 19:06:42 +0200

    FIn Ejercicio_11

---
+++ src/Ejercicio_11.java
@@ -0,0 +1,26 @@
+public class Ejercicio_11 {
+    public static void main(String[] args) {
+        for (int num = 1; num <= 8; num++) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println("\n");
+
+        for (int num = 1; num <= 9; num+= 2) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println("\n");
+
+        for (int num = 9; num >= 1; num-= 1) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println("\n");
+
+        for (int num = 9; num >= 1; num-= 2) {
+            System.out.print(num + " ");
+        }
+
+    }
+}
