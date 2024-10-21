71b97d2da95c1d5ad7e09c11f68967d72d053df9
Franco <juafracas@alu.edu.gva.es>
2024-10-21 17:27:03 +0200

    FIn Ejercicio_12

---
+++ src/Ejercicio_12.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ejercicio_12 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int total;
+
+        System.out.println("Ingresa un numero: ");
+        int numero = scan.nextInt();
+
+        for (int numero2 = 1; numero2 <= 10; numero2++){
+            total = numero * numero2;
+            System.out.println(numero + " x " + numero2 + " = " + total);
+         }
+    }
+}
