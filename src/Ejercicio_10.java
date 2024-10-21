d6590fffad9e1c399caa3445749eb6d507d3010e
Franco <juafracas@alu.edu.gva.es>
2024-10-21 17:19:49 +0200

    FIn Ejercicio_10

---
+++ src/Ejercicio_10.java
@@ -0,0 +1,13 @@
+public class Ejercicio_10 {
+    public static void main(String[] args) {
+
+        int suma = 0;
+
+        for (int cont = 0 ; cont <= 5; cont++) {
+            suma = suma + cont;
+            System.out.println(cont + " ");
+        }
+        System.out.println("La suma es " + suma);
+
+    }
+}
